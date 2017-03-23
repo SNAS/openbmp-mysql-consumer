8ff5ffa1c14fc054f8328d34da4e624ee177d88b
Andrew Gallo <agallo@gwu.edu>
2017-03-23 17:34:07 -0400

    adding usage comment in procedure

--- database/mysql-openbmp-proc-deleterouter.sql
+++ database/mysql-openbmp-proc-deleterouter.sql
@@ -3,6 +3,7 @@ DROP PROCEDURE IF EXISTS DeleteRouter;
 CREATE PROCEDURE DeleteRouter
 (IN rname varchar(200))
 BEGIN
+/* usage : DeleteRouter('OldRouter'), where OldRouter is the name of the device no longer sending BMP messages to the collector */ 
   DECLARE hid char(32);
   SELECT hash_id INTO hid FROM routers WHERE name = rname;
   DELETE r FROM rib r JOIN bgp_peers p ON (r.peer_hash_id = p.hash_id) WHERE p.router_hash_id = hid;
